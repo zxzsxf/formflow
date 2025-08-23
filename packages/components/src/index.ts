8144dd29198c1d6b925b7dbde71ff2ff4239b5c1
szy <187384130@qq.com>
2025-08-23 21:24:17 +0800

    fix: 引用问题

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -22,4 +22,4 @@ export * from './components/FieldDebug';
 export * from './components/PreviewText';
 
 // 默认导出
-export { default as FlowForm } from './components/FlowForm'; 
+export { default as FlowForm } from './components/FormFlow'; 
