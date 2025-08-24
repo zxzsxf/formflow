4f46825dd71da3a5cac8d010543d118babe7dd68
szy <187384130@qq.com>
2025-08-24 16:11:29 +0800

    fix: mobile打包问题修复 && workspace引入flowform/component

--- packages/mobile/rollup.config.mjs
+++ packages/mobile/rollup.config.mjs
@@ -2,9 +2,14 @@ import { defineConfig } from 'rollup';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
-  input: 'src/index.ts',
+  input: 'src/index.tsx',
   output: [
     {
       file: 'dist/index.js',
@@ -17,10 +22,28 @@ export default defineConfig({
       sourcemap: true
     }
   ],
-  external: ['react', 'react-dom', '@flowform/components', '@flowform/shared'],
+  external: [
+    'react', 
+    'react-dom', 
+    '@flowform/shared',
+    '@formily/antd',
+    '@formily/core',
+    '@formily/react',
+    '@formily/reactive',
+    '@formily/reactive-react',
+    '@formily/shared',
+    'antd'
+  ],
   plugins: [
-    nodeResolve(),
-    commonjs(),
+    nodeResolve({
+      alias: {
+        '@flowform/components': resolve(__dirname, '../components/dist')
+      },
+      extensions: ['.js', '.jsx', '.ts', '.tsx']
+    }),
+    commonjs({
+      ignore: ['@formily/antd/**/*.less', 'antd/**/*.less']
+    }),
     typescript({
       tsconfig: './tsconfig.json'
     })
--- packages/mobile/src/index.tsx
+++ packages/mobile/src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import BaseForm from './core/BaseForm';
-import { TestForm } from './render';
 
-ReactDOM.render(<TestForm formTemplateId='test123'/>, document.getElementById('root'));
+export { BaseForm };
+export default BaseForm;
